b16cce58539e9b69f4a2264c99182b300cf78098
Raeden <raider2891@gmail.com>
2026-01-16 04:43:00 +0600

    Initial commit

---
+++ src/main/java/com/raeden/hytale/HytaleEssentials.java
@@ -0,0 +1,17 @@
+package com.raeden.hytale;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class HytaleEssentials extends JavaPlugin {
+    public HytaleEssentials(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        
+    }
+}
