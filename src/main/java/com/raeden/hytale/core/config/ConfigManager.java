2c669803dfaa7deee1bb79189153d3d3fdc9e20d
Raeden <raider2891@gmail.com>
2026-01-17 04:39:01 +0600

    Added Config File

--- src/main/java/com/raeden/hytale/core/config/ConfigManager.java
+++ src/main/java/com/raeden/hytale/core/config/ConfigManager.java
@@ -6,20 +6,22 @@ import com.hypixel.hytale.logger.HytaleLogger;
 import com.raeden.hytale.HytaleEssentials;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 
 public class ConfigManager {
     private final HytaleLogger logger;
     private final HytaleEssentials hytaleEssentials;
 
-    private static final String CONFIG_FILE = "config.json";
-    private static final String LANG_FILE = "en-us.json";
-    private static final String RANK_CONFIG = "rank.json";
-    private static final String PARTY_CONFIG = "party.json";
-    private static final String ECONOMY_CONFIG = "economy.json";
-    private static final String CHAT_CONFIG = "chat.json";
+    private final String CONFIG_FILE = "config.json";
+    private final String LANG_FILE = "en-us.json";
+    private final String RANK_CONFIG = "rank.json";
+    private final String PARTY_CONFIG = "party.json";
+    private final String ECONOMY_CONFIG = "economy.json";
+    private final String CHAT_CONFIG = "chat.json";
+
+    private defaultConfig defaultConfig;
 
     private final Path dataDir;
     private final Gson gson;
@@ -33,16 +35,70 @@ public class ConfigManager {
 
     public void loadConfig() {
         try {
-            if(!Files.exists(dataDir, LinkOption.valueOf(CONFIG_FILE))) {
+            if(!Files.exists(dataDir)) {
                 Files.createDirectories(dataDir);
                 logger.atInfo().log("Created data folder: " + dataDir);
             }
+
+            this.defaultConfig = loadConfigData();
+
         } catch (IOException e) {
-            logger.atSevere().log("Failed to create config.json!");
-            createDefaultConfig();
+            logger.atSevere().log("Failed to create config.json in data directory!");
+            this.defaultConfig = createDefaultConfig();
         }
     }
 
-    private void createDefaultConfig() {
+    private defaultConfig loadConfigData() {
+        Path configFile = dataDir.resolve(CONFIG_FILE);
+        if(Files.exists(configFile)) {
+            try {
+                String readConfig = Files.readString(configFile, StandardCharsets.UTF_8);
+                defaultConfig config = gson.fromJson(readConfig, this.defaultConfig.getClass());
+
+                if(config == null) {
+                    logger.atSevere().log("Failed to read config.json from data directory!");
+                } else {
+                    return config;
+                }
+            } catch (IOException e) {
+                logger.atSevere().log("Failed to read config.json from data directory!");
+            }
+        }
+
+        defaultConfig defConfig = createDefaultConfig();
+        saveConfigFile(defConfig);
+        return defConfig;
+    }
+
+    private void saveConfigFile(defaultConfig config) {
+        Path savePath = dataDir.resolve("config.json");
+        String toJson = gson.toJson(config);
+        try {
+            Files.writeString(savePath, toJson, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            logger.atSevere().log("Failed to save config.json to data directory!");
+        }
+    }
+
+    private defaultConfig createDefaultConfig() {
+        defaultConfig config = new defaultConfig();
+
+        config.LANG = "en-us.json";
+        config.TOGGLE_ADMIN_MODULE = true;
+        config.TOGGLE_CHAT_MODULE = true;
+        config.TOGGLE_PARTY_MODULE = true;
+        config.TOOGLE_ECONOMY_MODULE = true;
+        config.TOGGLE_ANALYTICS_MODULE = true;
+
+        return config;
+    }
+
+    private static class defaultConfig {
+        private boolean TOGGLE_ADMIN_MODULE;
+        private boolean TOGGLE_CHAT_MODULE;
+        private boolean TOGGLE_PARTY_MODULE;
+        private boolean TOOGLE_ECONOMY_MODULE;
+        private boolean TOGGLE_ANALYTICS_MODULE;
+        private String LANG;
     }
 }
--- src/main/java/com/raeden/hytale/utils/generalUtils.java
+++ src/main/java/com/raeden/hytale/utils/generalUtils.java
@@ -1,13 +1,59 @@
 package com.raeden.hytale.utils;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.NameMatching;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
+import com.hypixel.hytale.server.core.inventory.container.ItemContainer;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.Universe;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
+import static com.hypixel.hytale.server.core.entity.entities.Player.*;
 import static com.raeden.hytale.HytaleEssentials.myLogger;
 
 public class generalUtils {
-    private static PlayerRef findPlayerByName(String username) {
+    public static boolean playerHasInventorySpace(PlayerRef playerRef, int slots) {
+        if(slots <= 0) {
+            return true;
+        }
+
+        try {
+            if(playerRef == null) {
+                return false;
+            }
+
+            Ref<EntityStore> entityStoreRef = playerRef.getReference();
+            if(entityStoreRef != null && entityStoreRef.isValid()) {
+                Store<EntityStore> store = entityStoreRef.getStore();
+                Player player = store.getComponent(entityStoreRef, Player.getComponentType());
+                if(player == null) {
+                    return false;
+                } else {
+                    ItemContainer inventory = player.getInventory().getCombinedEverything();
+                    int availableSlots = 0;
+                    short totalSlots = inventory.getCapacity();
+
+                    for(short i = 0; i < totalSlots; ++i) {
+                        ItemStack item = inventory.getItemStack(i);
+                        if(item == null || item.isEmpty()) {
+                            ++availableSlots;
+                        }
+                    }
+
+                    return availableSlots >= slots;
+                }
+            } else {
+                return false;
+            }
+        }  catch (Exception e) {
+            myLogger.atWarning().log("Error checking inventory slots for player " + playerRef.getUsername(), e);
+            return false;
+        }
+    }
+
+    public static PlayerRef findPlayerByName(String username) {
         try {
             Universe universe = Universe.get();
             if(universe == null) {
@@ -29,6 +75,10 @@ public class generalUtils {
     public static boolean isPlayerOnline(String username) {
         try {
             PlayerRef playerRef = findPlayerByName(username);
+            return playerRef != null && playerRef.getReference() != null && playerRef.getReference().isValid();
+        } catch (Exception e) {
+            myLogger.atWarning().log("Error checking if player is online: " + username + " - " + e.getMessage());
+            return false;
         }
     }
 }
